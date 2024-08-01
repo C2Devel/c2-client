38a36a1fd4d8a39b54a7385105eb77fd09a65fde
Gleb Pilikin <gpilikin@croc.ru>
2024-08-01 19:37:54 +0300

    c2client: refactor the scheme getting for the parameter

--- c2client/utils.py
+++ c2client/utils.py
@@ -1,13 +1,22 @@
+import dataclasses
 import os
-from typing import Any
+from typing import Any, Dict
 
 from botocore.model import ListShape, StructureShape, Shape
 
 from c2client.errors import EnvironmentVariableError, MalformedParametersError
 
 
+@dataclasses.dataclass
+class Parameter:
+    """API parameter info."""
+
+    name: str
+    shape: Shape
+
+
 def from_dot_notation(source):
-    """Converts a incoming query to a request dictionary.
+    """Convert a incoming query to a request dictionary.
     For example::
         1. {"Action": ["Action"], "Param": ["Value"]}
         2. {"Action": ["Action"], "Param.2": ["Value2"], "Param.1": ["Value1"]}
@@ -19,6 +28,7 @@ def from_dot_notation(source):
     :type query: dict
     :param query: This is dictionary, returned by '_get_query()'.
     """
+
     result = {"result": {}}
     for key, value in sorted(source.items()):
         try:
@@ -48,8 +58,8 @@ def _process_tokens(tokens, value, parent, index):
         parent[index][key] = value
 
 
-def get_env_var(name):
-    """Returns env_var by it's name or raises EnvironmentError."""
+def get_env_var(name: str) -> Any:
+    """Return env_var by it's name or raises EnvironmentError."""
 
     env_var = os.environ.get(name)
     if env_var is None:
@@ -57,31 +67,40 @@ def get_env_var(name):
     return env_var
 
 
+def collect_param_shapes(shape: Shape) -> Dict[str, Parameter]:
+    """Collect a dict of API schema parameters by their name or serialization name."""
+
+    member_shapes = {}
+    for member in shape.members:
+        member_shapes[member] = Parameter(name=member, shape=shape.members[member])
+        if shape.members[member].serialization.get("name"):
+            member_shapes[shape.members[member].serialization.get("name")] = Parameter(
+                name=member, shape=shape.members[member])
+
+    return member_shapes
+
+
 def convert_args(params: Any, shape: Shape):
-    """Converts values in the params dictionary to the types expected by shape."""
+    """Convert values in the params dictionary to the types expected by shape."""
 
     if not isinstance(shape, StructureShape):
         return convert_arg(value=params, shape=shape)
 
     converted_params = {}
+    param_shapes = collect_param_shapes(shape)
 
     for param_name, param_value in params.items():
-        if param_name in shape.members:
-            member_shape = shape.members[param_name]
-            converted_params[param_name] = convert_arg(param_value, member_shape)
-        elif f"{param_name}s" in shape.members:
-            # NOTE:  use the plural when looking for the shape of parameter
-            param_name = f"{param_name}s"
-            member_shape = shape.members[param_name]
-            converted_params[param_name] = convert_arg(param_value, member_shape)
+        parameter = param_shapes.get(param_name)
+        if parameter.shape:
+            converted_params[parameter.name] = convert_arg(param_value, parameter.shape)
         else:
-            converted_params[param_name] = param_value
+            converted_params[parameter.name] = param_value
 
     return converted_params
 
 
 def convert_arg(value: Any, shape: Shape):
-    """Converts an individual value to the type expected by shape."""
+    """Convert an individual value to the type expected by shape."""
 
     if isinstance(shape, ListShape):
         if not isinstance(value, list):
@@ -93,17 +112,17 @@ def convert_arg(value: Any, shape: Shape):
             raise ValueError(f"Expected dict for {shape.name}, got {type(value).__name__}")
         return convert_args(value, shape)
 
-    elif shape.type_name == 'string':
+    elif shape.type_name == "string":
         return str(value)
 
-    elif shape.type_name == 'integer' or shape.type_name == "long":
+    elif shape.type_name == "integer" or shape.type_name == "long":
         return int(value)
 
-    elif shape.type_name == 'float' or shape.type_name == "double":
+    elif shape.type_name == "float" or shape.type_name == "double":
         return float(value)
 
-    elif shape.type_name == 'boolean':
-        if isinstance(value, str) and value.lower() == 'false':
+    elif shape.type_name == "boolean":
+        if isinstance(value, str) and value.lower() == "false":
             return False
         return bool(value)
     else:
